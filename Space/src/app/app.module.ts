126a53a9c6d5970ac865b6b8a5752607c7d252e8
joseph <josepharaujo123@gmail.com>
2022-12-29 17:37:59 -0500

    test1

--- Space/src/app/app.module.ts
+++ Space/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TechnologyModule } from "./technology/technology.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        TechnologyModule
+    ]
 })
 export class AppModule { }
